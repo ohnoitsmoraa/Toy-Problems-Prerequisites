0d2ca9819e32206169df93e33133780320c711a0
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-07-03 10:38:17 +0300

    Modify speed.js

--- speed.js
+++ speed.js
@@ -1,10 +1,15 @@
 function speedDetector (speed){
+
+    const speed = prompt (`Enter speed of car`)
+
 const speedLimit = 70;
+
 const demeritPoints = (( speed - speedLimit)/5);
-if (speed <= 70){
-    console.log ("Ok");
+
+if (speed < 70){
+    console.log (`Ok`);
 }else if (demeritPoints >= 12){
-    console.log ("License suspended");
+    console.log (`License suspended`);
 }else {
     console.log (`Points ${demeritPoints}`);
 }
